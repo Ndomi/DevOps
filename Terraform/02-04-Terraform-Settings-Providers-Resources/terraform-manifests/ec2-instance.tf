f3647189f98252c0eb39ec30813c39985bc193be
Soho <jenkins@sohotech.io>
2021-08-20 21:44:57 +0200

    Create basic EC2 instance

--- Terraform/02-04-Terraform-Settings-Providers-Resources/terraform-manifests/ec2-instance.tf
+++ Terraform/02-04-Terraform-Settings-Providers-Resources/terraform-manifests/ec2-instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "mi-0c2b8ca1dad447f8a"
+  ami = "ami-0c2b8ca1dad447f8a"
   instance_type = "t3.micro"
   user_data = file("${path.module}/apache.sh")
 
