ef9abb550cc0f548da7d9330bf7dac84fe62ebd0
Soho <jenkins@sohotech.io>
2021-08-21 15:46:18 +0200

    input variables, datasorce and output values

---
+++ Terraform/02-05-Terraform-Variables-Datasource/c6-output.tf
@@ -0,0 +1,9 @@
+output "instance_publicip" {
+  description = "EC2 Instance Public IP"
+  value =  aws_instance.myec2vm.public_ip
+}
+
+output "instance_publicdns" {
+  description = "EC2 instance Public DNS"
+  value = aws_instance.myec2vm.public_dns
+}
