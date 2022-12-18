5ed491adf83face9160fad48e0e75d7ddd622705
ndomi <n.nqeketo@yahoo.co.za>
2022-12-18 16:57:33 +0200

    jenkins server

---
+++ Terraform/Terraform_Jenkins/jenkins.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo yum update
+sudo yum install wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+sudo yum install git -y
