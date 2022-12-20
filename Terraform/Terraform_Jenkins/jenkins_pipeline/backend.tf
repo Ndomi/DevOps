a9c8d3bc8e35ef708e13917b27af99673e307b02
ndomi <n.nqeketo@yahoo.co.za>
2022-12-20 18:41:48 +0200

    Remove the Format stage v2

--- Terraform/Terraform_Jenkins/jenkins_pipeline/backend.tf
+++ Terraform/Terraform_Jenkins/jenkins_pipeline/backend.tf
@@ -1,28 +1,28 @@
 resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
-    name = "terraform-lock"
-    hash_key = "LockID"
-    read_capacity = 5
-    write_capacity = 5
+  name           = "terraform-lock"
+  hash_key       = "LockID"
+  read_capacity  = 5
+  write_capacity = 5
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-    tags = {
-      "Name" = "terraformLock Table"
-    }
+  tags = {
+    "Name" = "Terraform Lock Table"
+  }
 }
 
 terraform {
   backend "s3" {
-    bucket = "jenkins-state-20221214"
-    region = "us-east-1"
-    key = "terraform.tfstate"
+    bucket         = "jenkins-state-20221214"
+    region         = "us-east-1"
+    key            = "terraform.tfstate"
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = var.profile
 }
